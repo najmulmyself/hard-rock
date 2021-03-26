c300705e599b2f3c287ca86ac02dca19540b4335
Najmul Huda <najmul.myself@gmail.com>
2021-03-26 23:20:06 +0600

    hard rock done

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const searchBtn = () => {
 }
 
 const displaySongs = songs => {
+    let songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = ''; // to clean the previous search result ;
     songs.forEach(song => {
-        let songContainer = document.getElementById('song-container');
         let Div = document.createElement('div');
         Div.className = 'single-result row align-items-center my-3 p-3';
         Div.innerHTML = `<div class="col-md-9">
@@ -16,9 +17,23 @@ const displaySongs = songs => {
             <p class="author lead">Album by <span>${song.artist.name}</span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
-            <button class="btn btn-success">Get Lyrics</button>
+            <button onClick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
+            <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+            </audio>
         `;
         songContainer.appendChild(Div);
     });
+}
+
+let getLyric = (artist,title) =>{
+    let url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let showLyric = document.getElementById('lyrics');
+        showLyric.innerText = data.lyrics;
+    }
+    )
 }
